a8aed24d232aff5aee9fcef497a0d921642c26d3
Keerthan Kumar C <ckeerthankumar4@gmail.com>
2024-10-11 17:59:04 +0530

    production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   webpack: (config) => {
-    // See https://webpack.js.org/configuration/resolve/#resolvealias
     config.resolve.alias = {
       ...config.resolve.alias,
       sharp$: false,
