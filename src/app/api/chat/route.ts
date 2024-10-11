9a329a63f187f96d4386d7b7cf481878f12f7107
Keerthan Kumar C <ckeerthankumar4@gmail.com>
2024-10-11 17:52:08 +0530

    bug fix

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,26 +1,15 @@
-// import {google} from '@ai-sdk/google'
-// import { queryPineconeVectorStore } from "@/utils";
+
 import { Pinecone } from "@pinecone-database/pinecone";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { Message, StreamData, streamText } from "ai";
-//import { PineconeStore } from "@langchain/pinecone";
-// import { Document } from "@langchain/core/documents";
-//import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/hf_transformers";
 
 import { queryPineconeVectorStore } from "@/app/utils";
 
 export const maxDuration = 60;
-// export const runtime = 'edge';
 
 const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY ?? "",
 });
-// const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME!);
-
-// const EmbeddingModel = new HuggingFaceTransformersEmbeddings({
-//   model: "Xenova/all-MiniLM-L6-v2",
-// });
-
 const google = createGoogleGenerativeAI({
   baseURL: "https://generativelanguage.googleapis.com/v1beta",
   apiKey: process.env.GEMINI_API_KEY,
@@ -31,36 +20,19 @@ const model = google("models/gemini-1.5-pro-latest", {
     { category: "HARM_CATEGORY_DANGEROUS_CONTENT", threshold: "BLOCK_NONE" },
   ],
 });
-export async function POST(req: Request) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function POST(req: Request, res: Response) {
   const reqBody = await req.json();
   console.log("request body is \n", reqBody);
 
   const messages: Message[] = reqBody.messages;
   console.log("messages are \n", messages);
   const userQuestion = `${messages[messages.length - 1].content}`;
-  // const userQuestion = messages
-  //   .filter((msg) => msg.role === "user") // Keep only messages from the user
-  //   .map((msg) => `${msg.content}`) // Map to their content
-  //   .join("\n");
-  console.log("user question is \n", userQuestion);
-  console.log("creating vector store \n");
-  // const vectorStore = await PineconeStore.fromExistingIndex(EmbeddingModel, {
-  //   pineconeIndex,
-  //   maxConcurrency: 5,
-  // });
-  console.log("vector store created \n");
-  console.log("retriever creation started \n");
-  // const retriever = vectorStore.asRetriever({
-  //   searchType: "mmr",
-  //   k: 3,
-  // });
-  console.log("retriever creation done \n");
-  console.log("getting relevant documents \n");
-  // const docs = await retriever.invoke(userQuestion);
-  console.log("relevant docs got \n");
-  // const context = "";
-  // docs.map((m) => context.concat(m.pageContent));
-  const context = await queryPineconeVectorStore(pinecone, 'dlprojectchecknomic',userQuestion);
+  const context = await queryPineconeVectorStore(
+    pinecone,
+    "dlprojectchecknomic",
+    userQuestion
+  );
   console.log("context is \n", context);
 
   const rag_prompt = `You are an intelligent assistant designed to provide accurate and relevant information from Keras documentation.
